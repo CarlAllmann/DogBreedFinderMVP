bfc77ea43139c67fdd56c2d1a015e0bc35fdee18
CarlAllmann <49835729+CarlAllmann@users.noreply.github.com>
2019-05-14 16:27:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict'
+$(document).ready(function() {
+    $(".search").hide();
+    $(".welcome").show();
+    $(".results").hide();
+    });
+
+
+
+
+
+
+
+
+
+
+
+function watchForm() {
+    }
+
+
+
+
+
+
